46d8735ddd13f69c2ecf881479131307c037116b
Muhammad Faizan <muhammadfaizan027915@gmail.com>
2026-05-17 12:22:43 +0500

    Setting up the CV

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = 5000;
 const ROOT = path.resolve(__dirname);
 
 const MIME = {
